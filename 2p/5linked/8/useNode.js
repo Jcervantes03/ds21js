ca7e8dc1a86306ba7d8ca9c2e57b696c8cb51151
CERVANTES RAMIREZ RAMIREZ <99361437+Jcervantes03@users.noreply.github.com>
2022-10-26 12:44:03 +0000

    1.1

--- 2p/5linked/8/useNode.js
+++ 2p/5linked/8/useNode.js
@@ -4,10 +4,10 @@
 import {LinkedList} from '../../../jlcr/linked/listasEnlazadas6es.js'
 
 let listaEnlazada = new LinkedList()
-listaEnlazada.append("m")
-listaEnlazada.append("r")
-listaEnlazada.append("k")
-console.log(listaEnlazada.insertAfter("m","k"))
-console.log(listaEnlazada.insertBefore("r","m"))
+listaEnlazada.append(5)
+listaEnlazada.append(6)
+listaEnlazada.append(9)
+console.log(listaEnlazada.insertAfter(9,5))
+console.log(listaEnlazada.insertBefore(5,6))
 listaEnlazada.print()
 
